f399ded2aeae951fa60ad05732fef354d945d884
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2022-07-31 15:10:17 -0400

    Update training.py

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 from transformers import ViTFeatureExtractor, ViTForImageClassification
 from PIL import Image
-import requests
+import requests'
+import streamlit as st
 
 
 def classified_image(url):
@@ -17,3 +18,37 @@ def classified_image(url):
     result = model.config.id2label[predicted_class_idx]
     print("Predicted class:", result)
     return result
+
+
+def run():
+    # Streamlit page title
+    st.title("Classify your Images here")
+    st.markdown('**Vision Transformer (ViT) model pre-trained on ImageNet-21k (14 million images, 21,843 classes) at resolution 224x224, and fine-tuned on ImageNet 2012 (1 million images, 1,000 classes) at resolution 224x224.**')
+    hide_footer_style = """<style>.reportview-container .main footer {visibility: hidden;}"""
+    st.markdown(hide_footer_style, unsafe_allow_html=True)
+
+    # Clear input form
+    def clearform():
+        st.session_state['newtext'] = ''
+
+    # Input form
+    with st.form('reviewtext'):
+        new_review = st.text_area(label='Give your Image URL',
+                                    value = '',
+                                    key='newtext')
+        b1,b2 = st.columns([1,1])
+        with b1:
+            submit = st.form_submit_button(label='Submit')
+        with b2:
+            clear = st.form_submit_button(label='Reset', on_click=clearform)
+
+    if submit and new_review !='':
+        # Generate prediction
+        result = classified_image(new_review)
+
+        # Display the prediction
+        st.markdown('### Predicted Class: {}'.format(result))
+
+
+if __name__ == "__main__":
+    run()
