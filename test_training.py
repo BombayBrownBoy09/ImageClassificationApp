2b21dca9995d9fd634a9e6da8fc9c9a67aa107fd
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2022-07-31 14:57:34 -0400

    Update test_training.py

--- test_training.py
+++ test_training.py
@@ -5,5 +5,5 @@ def test_classified_image():
     result = classified_image(
         "http://images.cocodataset.org/val2017/000000039769.jpg"
     )
-    assert result.lower.__contains__('cat')
+    assert result.lower().__contains__('cat')
   
